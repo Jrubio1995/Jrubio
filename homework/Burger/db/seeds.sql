17618eb6901629d7d9b83c2a7430067a32462d4e
JesusRubio <jesusarizarubio@gmail.com>
2020-10-09 19:00:04 -0500

    adding burger folder

---
+++ homework/Burger/db/seeds.sql
@@ -0,0 +1,6 @@
+--Adding some presets values for the app.
+USE burgers_db
+
+INSERT INTO burgers(burgers_name, devoured)
+
+VALUES ("hambugers",false), ("cheeseburger",false), ("avcadoburger",false);
---
+++ homework/Burger/public/js/burger.js
@@ -0,0 +1,51 @@
+$(function() {
+    $(".create-form").on("submit", function(event) {
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#newburger")
+          .val()
+          .trim(),
+        devoured: 0
+      };
+  
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(function() {
+        console.log("Adding to new burger list");
+        location.reload();
+      });
+    });
+  
+    //Devouring
+    $(".eatburger").on("click", function(event) {
+      event.preventDefault();
+  
+      var id = $(this).data("id");
+      var devouring = {
+        devoured: 1
+      };
+  
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: devouring
+      }).then(function() {
+        console.log("Burger devoured aka eating");
+        location.reload();
+      });
+    });
+  
+    //Removing
+    $(".trashburger").on("click", function(event) {
+      event.preventDefault();
+  
+      var id = $(this).data("id");
+  
+      // Deletes from page
+      $.ajax({
+        type: "DELETE",
+        url: "/api/burgers/" + id
+      }).then(location.reload());
+    });
+  });
